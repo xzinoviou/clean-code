62cf8d278ba9c4652985016baedc61625e42f63c
xzinoviou <xzinoviou@gmail.com>
2019-07-08 23:13:09 +0300

    added functionality to Args.java

--- src/main/java/clean/code/Args.java
+++ src/main/java/clean/code/Args.java
@@ -1,10 +1,58 @@
 package clean.code;
 
+import java.text.ParseException;
+import java.util.*;
+
 /**
  * @author xzinoviou
  * created 8/7/2019
  */
 public class Args {
 
+    private String schema;
+    private String[] args;
+    private boolean valid = true;
+    private Set<Character> unexpectedArguments = new TreeSet<>();
+    private Map<Character, Boolean> booleanArgs = new HashMap<>();
+    private Map<Character, String> stringArgs = new HashMap<>();
+    private Map<Character, Integer> intArgs = new HashMap<>();
+    private Set<Character> argsFound = new HashSet<>();
+    private int currentArgument;
+    private char errorArgumentId = '\0';
+    private String errorParameter = "TILT";
+    private ErrorCode errorCode = ErrorCode.OK;
+
+    private enum ErrorCode {
+        OK, MISSING_STRING, MISSING_INTEGER, INVALID_INTEGER, UNEXPECTED_ARGUMENT
+    }
+
+    public Args(String shcema, String[] args) {
+        this.schema = schema;
+        this.args = args;
+        valid = parse();
+    }
+
+    private boolean parse() throws ParseException {
+        if (schema.length() == 0 && args.length == 0)
+            return true;
+        parseSchema();
+
+        try {
+            parseArguments();
+        } catch (ArgsException e) {
+
+        }
+        return valid;
+    }
+
+    private boolean parseSchema() throws ParseException {
+        for (String element : schema.split(",")) {
+            if (element.length() > 0) {
+                String trimmedElement = element.trim();
+                parseSchemaElement(trimmedElement);
+            }
+        }
+        return true;
+    }
 
 }
